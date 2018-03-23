ebbbccfda6907bbc3b14374e7e830b603c135023
Prasul Surendran <prasul@gmail.com>
2018-03-23 18:39:05 +0530

    Adding hello world

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Prasul Surendran on 23/03/18.
+//  Copyright © 2018 Prasul Surendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

aceb65a2a4d4a8f5694b77bf8f158f1f72641a43
Prasul Surendran <prasul@gmail.com>
2018-04-18 23:06:54 +0530

    updating i am poor and i am rich

---
+++ Iampoor/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Prasul Surendran on 16/04/18.
+//  Copyright © 2018 Prasul Surendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

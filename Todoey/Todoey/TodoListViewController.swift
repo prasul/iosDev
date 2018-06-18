3b20b2bf54b402099f65b016fe07252601f9a97e
Prasul Surendran <prasul@gmail.com>
2018-06-18 08:22:43 +0530

    add new items to table and updates

--- Todoey/Todoey/TodoListViewController.swift
+++ Todoey/Todoey/TodoListViewController.swift
@@ -12,18 +12,18 @@ class TodoListViewController: UITableViewController {
     
     //MARK: Define Variables
     
-    let messageArray = ["Go to bathroom", "turn off the shower", "turn off the lights"]
-
+    var messageArray = ["Go to bathroom", "turn off the shower", "turn off the lights"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
     //MARK: Table View datasource methods
     
@@ -39,7 +39,7 @@ class TodoListViewController: UITableViewController {
     //MARK:- Define Delegates
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print("The row \(indexPath.row) is now selected and the value is \(messageArray[indexPath.row])")
+        //        print("The row \(indexPath.row) is now selected and the value is \(messageArray[indexPath.row])")
         
         tableView.deselectRow(at: indexPath, animated: true)
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
@@ -48,6 +48,33 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
     }
-
+    
+    //MARK:- Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textFieldData = UITextField()
+        let alertItem = UIAlertController(title: "Add Items", message: "Add new items to ToDo", preferredStyle: .alert)
+        alertItem.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new ToDo"
+            textFieldData = alertTextField
+        }
+        let action = UIAlertAction(title: "Add new Item", style: .default) { (buttonPressed) in
+            self.messageArray.append(String(textFieldData.text!))
+            self.tableView.reloadData()
+        }
+        alertItem.addAction(action)
+        present(alertItem, animated: true, completion: nil)
+    }
+    
+    //MARK: Configuretable View
+    func configureTableView(){
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120.0
+    }
+    
+    //MARK: Retreive messages
+    
+    
+    
 }
 
